75b7fe551e98743ef63470b0dd285246b9183eeb
simidawalang <simidawalang@gmail.com>
2022-02-06 16:28:14 +0100

    first commit

---
+++ compile.js
@@ -0,0 +1,21 @@
+// const path = require('path');
+// const fs = require('fs');
+// const solc = require('solc');
+
+// const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+// const source = fs.readFileSync(inboxPath, 'utf8');
+
+// module.exports = solc.compile(source, 1).contracts[':Inbox'];
+
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf8');
+// utf8 is the encoding used for the file
+
+module.exports = solc.compile(source, 1).contracts[':Inbox'];
+console.log(module.exports);
+// second parameter in the compile function is used to 
+// specify the number of different contracts we're attempting to compile
